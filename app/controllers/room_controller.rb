ff3c28033d25fab23449656c927a35b49ec1b091
fuJiin <fu7iin@gmail.com>
2011-10-01 00:32:53 -0700

    logic for creating votes, vibes, question, room

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,14 +1,19 @@
 class RoomController < ApplicationController
-  
   def index
     @rooms = Room.all
   end
   
+  def create
+    require_params :name
+    @room = Room.find_or_create_by_name params[:name]
+    redirect_to :action => :teacher, :id => @room.id
+  end
+  
   def teacher
-
+    @room = Room.find params[:id]
   end
   
   def student
-    
+    @room = Room.find params[:id]
   end
 end
